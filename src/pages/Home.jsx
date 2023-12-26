a8019d870eda451a0113b6b5919ca37c7ed27d77
FerArj <fernando.araujo@sptech.school>
2023-12-26 12:58:36 -0300

    config e estrutura inicial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1 className="text-x1 font-bold text-red-600">Home</h1>
+            <p>Home page content</p>
+        </>
+    )
+}
+
+export default Home

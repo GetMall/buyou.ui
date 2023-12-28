b48ff238e6a1082aab896e1d84e267146e5c76c9
FerArj <fernando.araujo@sptech.school>
2023-12-28 01:37:31 -0300

    retirado section como funciona

--- src/pages/Institucional/Home.jsx
+++ src/pages/Institucional/Home.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header"
-import imgConta from "../../assets/institucional/img_section1.svg"
-import imgSobreNos from "../../assets/institucional/img_section3.svg"
-import imgGerencie from "../../assets/institucional/img_section4.svg"
+import imgConta from "../../assets/institucional/img_shopping-mulher.svg"
+import imgSobreNos from "../../assets/institucional/img_mulher.svg"
+import imgGerencie from "../../assets/institucional/img_entrega.svg"
 
 function Home() {
     return (
@@ -31,29 +31,6 @@ function Home() {
                         <p className="mt-8 text-lg">Com a GetShop, você conecta seu restaurante a milhões de novos clientes, expande sua área de entrega e vende muito mais.</p>
                         <button className="bg-primary text-white p-3 rounded-md mt-8">Gerencie sua loja!</button>
                 </div>
-                </section>
-                <section className="flex flex-col items-center mt-20">
-                    <h1 className="text-5xl text-primary  font-bold">Como funciona?</h1>
-                    <div className="flex mt-20">
-                        <div className="flex flex-col items-center">
-                            <div className="bg-primary rounded-full w-32 h-32 flex justify-center items-center">
-                                <p className="text-white text-5xl">1</p>
-                            </div>
-                            <p className="text-2xl mt-8">Você escolhe o produto...</p>
-                        </div>
-                        <div className="flex flex-col items-center ml-36">
-                            <div className="bg-primary rounded-full w-32 h-32 flex justify-center items-center">
-                                <p className="text-white text-5xl">2</p>
-                            </div>
-                            <p className="text-2xl mt-8">Nós encontramos o Shopping mais próximo...</p>
-                        </div>
-                        <div className="flex flex-col items-center ml-36">
-                            <div className="bg-primary rounded-full w-32 h-32 flex justify-center items-center">
-                                <p className="text-white text-5xl">3</p>
-                            </div>
-                            <p className="text-2xl mt-8">Entregamos em menos de 1 hora!</p>
-                        </div>
-                    </div>
                 </section>
             </main>
         </>

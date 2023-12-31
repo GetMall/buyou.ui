f2c3f2df75c56a9ed49bef38fc693f4e6746a876
FerArj <fernando.araujo@sptech.school>
2023-12-31 14:50:12 -0300

    criado card de login

--- src/pages/Institucional/Acesso.jsx
+++ src/pages/Institucional/Acesso.jsx
@@ -1,11 +1,25 @@
 import bgAcesso from '../../assets/institucional/acesso/bg_acesso.svg'
 
-function Acesso () {
+function Acesso() {
     return (
         <>
             <div>
-                <img className='h-screen' src={bgAcesso} alt=""/>
-                
+                <div className='flex w-96 justify-center absolute mt-60 ml-60'>
+                    <div className='bg-white absolute shadow-lg ml-96 left-96 justify-center'>
+                        <div className='p-12 flex flex-col items-center justify-center'>
+                            <h1 className='font-bold size-max text-3xl'>Que bom ver você aqui!</h1>
+                            <h2 className='size-max mt-3 mb-3'>Como deseja continuar?</h2>
+                            <div className='flex flex-col w-96 gap-5'>
+                                <input className='outline-none p-2  border-b_gray border-2' type="text" placeholder='Insira seu Email'/>
+                                <input className='outline-none p-2 border-b_gray border-2' type="password" placeholder='Insira sua senha' />
+                                <button className='bg-btn_orange text-white p-2 font-bold cursor-pointer'>Acessar a GetShop!</button>
+                                <p className='flex text-center justify-center'>Ou</p>
+                                <button className='bg-white text-btn_orange p-2 font-bold shadow cursor-pointer'>Acessar a GetShop!</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img className='h-screen' src={bgAcesso} alt="" />
             </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ export default {
       colors: {
         primary: '#559EA8',
         white_opacity: '#F7F7F7',
-        dark_primary: '#498A93'
+        dark_primary: '#498A93',
+        b_gray: '#E0E0E0',
+        btn_orange: '#FF530B',
       },
       fontFamily: {
         'sans': ['Roboto', 'sans-serif'],

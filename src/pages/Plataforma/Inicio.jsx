1b33360835dd5ba522a23560f5bb5cc6a6965f36
FerArj <fernando.araujo@sptech.school>
2024-01-13 18:35:04 -0300

    exibindo shopping e loja da api

--- src/pages/Plataforma/Inicio.jsx
+++ src/pages/Plataforma/Inicio.jsx
@@ -1,37 +1,60 @@
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
-
+import api from "../../services/api";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Inicio() {
+    const [shopping, setShopping] = useState([])
+    const [loja, setLoja] = useState([])
+
+    const getShopping = () => {
+        api.get('/shopping').then(response => {
+            setShopping(response.data)
+        }).catch(error => {
+            console.log(error)
+        });
+    }
+
+    const getLoja = () => { 
+        api.get('/lojas').then(response => {
+            setLoja(response.data)
+        }).catch(error => {
+            console.log(error)
+        });
+    }
+
+    useEffect(() => {
+        getShopping(),
+        getLoja()
+    }, [])
+
     return (
         <>
             <Header />
             <div className='flex p-5 gap-3 flex-col pl-20 mt-36'>
                 <h2 className='text-xl'>Próximos da sua região</h2>
-                <div className="flex gap-5">
-                    <Card />
-                    <Card />
+                <div className="flex flex-wrap gap-5">
+                    {shopping.map((shopping) => (
+                        <Card key={shopping.id} nomeLoja={shopping.nome} />
+                    ))}
                 </div>
             </div>
             <div className='flex p-5 gap-3 flex-col pl-20 mt-36'>
                 <h2 className='text-xl'>Shoppings Populares</h2>
-                <div className="flex gap-5">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                <div className="flex flex-wrap gap-5">
+                    {shopping.map((shopping) => (
+                        <Card key={shopping.id} nomeLoja={shopping.nome} />
+                    ))}
                 </div>
             </div>
             <div className='flex p-5 gap-3 flex-col pl-20 mt-36'>
                 <h2 className='text-xl'>Lojas Populares</h2>
-                <div className="flex gap-5">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                <div className="flex flex-wrap gap-5">
+                    {loja.map((loja) => (
+                        <Card key={loja.id} nomeLoja={loja.nome} />
+                    ))}
                 </div>
             </div>
             <Footer />
--- src/pages/Plataforma/components/Card.jsx
+++ src/pages/Plataforma/components/Card.jsx
@@ -1,10 +1,10 @@
 import imgLoja from '../../../assets/plataforma/card/img-loja.svg'
 
-function Card () {
+function Card ({nomeLoja}) {
     return (
         <div className="flex flex-col w-80 cursor-pointer bg-white h-40 justify-center p-3 shadow-md">
             <img className='w-16' src={imgLoja} alt="" />
-            <p>Shopping Ibirapuera</p>
+            <p>{nomeLoja}</p>
             <p>30-40 min</p>
         </div>
     )

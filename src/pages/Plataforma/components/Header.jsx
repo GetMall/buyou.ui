dcff2808d2e5f059046f25d862c77acace38abe8
FerArj <fernando.araujo@sptech.school>
2024-01-14 17:38:15 -0300

    adicionado submenu no perfil

--- src/pages/Plataforma/components/Header.jsx
+++ src/pages/Plataforma/components/Header.jsx
@@ -3,9 +3,14 @@ import cart from '../../../assets/plataforma/icon-cart.svg'
 import profile from '../../../assets/plataforma/icon-profile.svg'
 import logout from '../../../assets/plataforma/icon-logout.svg'
 import local from '../../../assets/plataforma/icon-local.svg'
+import arrow from '../../../assets/plataforma/icon-arrow.svg'
+import uparrow from '../../../assets/plataforma/icon-uparrow.svg'
+import config from '../../../assets/plataforma/icon-config.svg'
+import { useState } from 'react'
 
 function Header() {
 
+    const [menuAberto, setMenuAberto] = useState(false)
     const nomeUsuario = sessionStorage.getItem('nomeUsuario')
 
     return (
@@ -23,13 +28,28 @@ function Header() {
                     </div>
                     <div className='flex gap-5'>
                         <img className='cursor-pointer' src={cart} alt="" />
-                        <div className='flex gap-2 cursor-pointer bg-white_opacity p-2'>
-                            <img className='cursor-pointer' src={profile} alt="" />
-                            <p className='cursor-pointer'>{nomeUsuario}</p>
+                        <div onClick={() => setMenuAberto(true)} className='flex justify-center items-center gap-2 cursor-pointer bg-white_opacity p-2'>
+                            <img src={profile} alt="" />
+                            <p>{nomeUsuario}</p>
+                            <img src={arrow} className='size-3' alt="" />
+                            {menuAberto && (
+                                <div className='absolute mt-36 bg-white rounded shadow-md'>
+                                    <div className='p-2 gap-2 flex items-center'>
+                                        <img className='size-4' src={config} alt="" />
+                                        <p>Configurações</p>
+                                    </div>
+                                    <div className='p-2 gap-2 flex items-center'>
+                                        <img className='size-6' src={logout} alt="" />
+                                        <p>Sair</p>
+                                    </div>
+                                    <div onClick={(e) => {e.stopPropagation(); setMenuAberto(false);}} className='p-2 flex justify-center'>
+                                        <img className='size-3' src={uparrow} alt="" />
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
-                <img className='cursor-pointer' src={logout} alt="" />
             </div>
         </header>
     )

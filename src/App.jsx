85e1c5473e067bc3b86125d8d1e23c4006c06678
FerArj <fernando.araujo@sptech.school>
2024-01-15 00:35:30 -0300

    exibindo todas as lojas referente ao shopping

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Institucional/Home'
 import Acesso from './pages/Institucional/Acesso'
 import Inicio from './pages/Plataforma/Inicio'
+import Shopping from './pages/Plataforma/Shopping'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/acesso" element={<Acesso />} />
         <Route path="/inicio" element={<Inicio />} />
+'       <Route path="/shopping/:idShopping/:nomeShopping" element={<Shopping />} />
       </Routes>
     </Router>
   )

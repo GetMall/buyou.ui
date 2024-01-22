373dda51f9e22cadac7fd98dedcc45f1378a96ba
FerArj <fernando.araujo@sptech.school>
2024-01-21 23:44:55 -0300

    menu lateral da tela de perfil do usuário

---
+++ src/pages/Plataforma/Perfil.jsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "./components/perfil/Sidebar"
+
+function Perfil () {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Perfil
---
+++ src/pages/Plataforma/components/perfil/Sidebar.jsx
@@ -0,0 +1,65 @@
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  ShoppingBagIcon,
+  UserCircleIcon,
+  HomeIcon,
+  ShoppingCartIcon,
+  InboxIcon,
+} from "@heroicons/react/24/solid";
+
+import { useNavigate } from "react-router-dom";
+
+export function Sidebar() {
+  const navigate = useNavigate();
+
+  return (
+    <Card className="h-[calc(108vh-5rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <div className="mb-2 p-4">
+        <Typography variant="h5" color="blue-gray">
+          Configurações da Conta
+        </Typography>
+      </div>
+      <List className="gap-5">
+        <ListItem className="gap-2">
+          <ListItemPrefix>
+            <UserCircleIcon className="h-5 w-5 text-secundary" />
+          </ListItemPrefix>
+          Dados de Cadastro
+        </ListItem>
+        <ListItem className="gap-2">
+          <ListItemPrefix>
+            <ShoppingBagIcon className="h-5 w-5 text-secundary" />
+          </ListItemPrefix>
+          Ultimos Pedidos
+        </ListItem>
+        <ListItem className="gap-2">
+          <ListItemPrefix>
+            <InboxIcon className="h-5 w-5 text-secundary" />
+          </ListItemPrefix>
+          Ajuda
+        </ListItem>
+        <ListItem className="gap-2">
+          <ListItemPrefix>
+            <ShoppingCartIcon className="h-5 w-5 text-secundary" />
+          </ListItemPrefix>
+          Minha Loja
+        </ListItem>
+        <ListItem onClick={() => navigate('/inicio')} className="gap-2">
+          <ListItemPrefix>
+            <HomeIcon className="h-5 w-5 text-secundary" />
+          </ListItemPrefix>
+          Voltar
+        </ListItem>
+      </List>
+    </Card>
+  );
+}

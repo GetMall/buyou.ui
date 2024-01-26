d6d905211e3b253dcf4ffb0d80895106131f3bd0
FerArj <fernando.araujo@sptech.school>
2024-01-26 14:42:58 -0300

    formulario de cadastro alterado para ser um componente

--- src/pages/Plataforma/Perfil.jsx
+++ src/pages/Plataforma/Perfil.jsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "./components/perfil/Sidebar"
+import FormCadastro from "./components/perfil/FormCadastro"
 import Container from "./components/perfil/Container"
 
 function Perfil () {
@@ -6,7 +7,9 @@ function Perfil () {
         <div className="flex">
             <Sidebar />
             <div className="flex w-full justify-center items-center">
-                <Container />
+                <Container>
+                    <FormCadastro />
+                </Container>
             </div>
         </div>
     )

4c9984d11517c111016af00af43afec4d7a58551
FerArj <fernando.araujo@sptech.school>
2024-01-26 18:38:26 -0300

    listando produtos no carrinho

--- src/pages/Plataforma/Carrinho.jsx
+++ src/pages/Plataforma/Carrinho.jsx
@@ -8,13 +8,15 @@ function Carrinho() {
         <>
             <Header />
             <div className="mt-40 pl-20">
-                <h2 className="text-xl font-bold">Seu Carrinho</h2>
-                <div className="flex mt-5 gap-5">
+                <h2 className="text-xl mb-2 font-bold">Seu Carrinho</h2>
+                <div className="overflow-y-auto" style={{maxHeight: '800px'}}>
+                <div className="flex flex-col mt-5 gap-5">
                     {carrinhoItens.map((item) => (
                         <div key={item.id}>
-                            <CardProdutoCart imgProduto={`http://localhost:8080/produtos/arquivo/${item.nomeArquivoSalvo}`} preco={item.valorUnitario} descricao={item.descricao} />
+                            <CardProdutoCart imgProduto={`http://localhost:8080/produtos/arquivo/${item.nomeArquivoSalvo}`} tamanho={item.tamanho} cor={item.cor} preco={item.valorUnitario} descricao={item.descricao} />
                         </div>
                     ))}
+                </div>
                 </div>
             </div>
         </>
--- src/pages/Plataforma/components/loja/CardProdutoCart.jsx
+++ src/pages/Plataforma/components/loja/CardProdutoCart.jsx
@@ -1,12 +1,22 @@
-function CardProdutoCart({imgProduto, preco, descricao, onClick}) {
+function CardProdutoCart({ imgProduto, preco, descricao, onClick, tamanho, cor }) {
     return (
-        <div className="w-48 cursor-pointer  justify-center  bg-slate-100">
-            <img className="w-full h-56 flex justify-center " src={imgProduto} alt="" />
+        <div className="w-1/3 cursor-pointer  justify-center  bg-slate-100">
+            <div className="flex">
+                <img className="w-56 h-56 flex justify-center " src={imgProduto} alt="" />
+                <div className="flex flex-col p-5">
+                    <h2 className="text-xl font-bold">Caracteristicas</h2>
+                    <p className="text-sm overflow-hidden line-clamp-1">{descricao}</p>
+                    <p className="text-sm overflow-hidden line-clamp-1">Tamanho: {tamanho}</p>
+                    <p className="text-sm overflow-hidden line-clamp-1">Cor: {cor}</p>
+                    <div className="mt-5">
+                    <h2 className="text-xl font-bold">Total</h2>
+                    <p className="font-bold text-lg">R$ {preco}</p>
+                    </div>
+                </div>
+            </div>
             <div className="p-5">
-                <p className="font-bold text-lg">R$ {preco}</p>
-                <p className="text-sm overflow-hidden line-clamp-1">{descricao}</p>
-                <div className="flex">
-                    <button onClick={onClick} className='bg-btn_orange text-xs text-white p-2 font-bold cursor-pointer h-8 w-full'>Remover do Carrinho</button>
+                <div className="flex justify-end">
+                    <button onClick={onClick} className='bg-btn_orange text-xs text-white p-2 font-bold cursor-pointer h-8'>Remover do Carrinho</button>
                 </div>
             </div>
         </div>

737bab9e2836b9eb783fa160319726b07ff0c97d
FerArj <fernando.araujo@sptech.school>
2024-02-13 13:56:30 -0300

    borda adicionada nos cards

--- src/pages/Plataforma/components/loja/CardProduto.jsx
+++ src/pages/Plataforma/components/loja/CardProduto.jsx
@@ -1,6 +1,6 @@
 function CardProduto({imgProduto, preco, descricao, onClick}) {
     return (
-        <div className="w-48 cursor-pointer  justify-center  bg-slate-100">
+        <div className="w-48 cursor-pointer  justify-center border  bg-slate-100">
             <img className="w-full h-56 flex justify-center " src={imgProduto} alt="" />
             <div className="p-5">
                 <p className="font-bold text-lg">R$ {preco}</p>

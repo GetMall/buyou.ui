e54198b3d44c610e273b326fd357472e6f2ea41d
FerArj <fernando.araujo@sptech.school>
2024-04-12 23:45:39 -0300

    retirando botão de compra do produto

--- src/pages/Plataforma/components/loja/CardProduto.jsx
+++ src/pages/Plataforma/components/loja/CardProduto.jsx
@@ -5,9 +5,9 @@ function CardProduto({ imgProduto, preco, descricao, onClick }) {
             <div className="p-5">
                 <p className="font-bold text-lg">R$ {preco}</p>
                 <p className="text-sm overflow-hidden line-clamp-1">{descricao}</p>
-                <div className="flex mt-10">
+                {/* <div className="flex mt-10">
                     <button onClick={onClick} className='bg-secundary rounded-sm hover:bg-btn_orange text-xs text-white p-2 font-bold cursor-pointer h-8 w-full'>Adicionar ao Carrinho</button>
-                </div>
+                </div> */}
             </div>
         </div>
     )

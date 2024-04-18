f13bc9df623a14ed1ae654ecf492d12ce4a6120a
FerArj <fernando.araujo@sptech.school>
2024-04-17 22:46:47 -0300

    adicionando pts livelo

--- src/pages/Plataforma/components/loja/CardProduto.jsx
+++ src/pages/Plataforma/components/loja/CardProduto.jsx
@@ -1,9 +1,17 @@
+import liveloLogo from '../../../../assets/plataforma/card/livelo-logo.png'
+
 function CardProduto({ imgProduto, preco, descricao, onClick }) {
     return (
         <div className="w-96 cursor-pointer rounded-sm justify-center border bg-white">
             <img className="w-full h-56 flex rounded-sm justify-center " src={imgProduto} alt="" />
             <div className="p-5">
+                <div className="flex justify-between">
                 <p className="font-bold text-lg">R$ {preco}</p>
+                <div className='flex justify-center items-center gap-2'>
+                <img className='w-[30px]' src={liveloLogo} alt="" />
+                <p className='text-[#e50091]'>{preco.toFixed(0)}<span>pts</span></p>
+                </div>
+                </div>
                 <p className="text-sm overflow-hidden line-clamp-1">{descricao}</p>
                 <div className="flex mt-10">
                     <button onClick={onClick} className='bg-secundary rounded-sm hover:bg-btn_orange text-xs text-white p-2 font-bold cursor-pointer h-8 w-full'>Adicionar ao Carrinho</button>
